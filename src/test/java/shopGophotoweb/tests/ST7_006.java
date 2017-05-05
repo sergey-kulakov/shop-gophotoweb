83ddf67a9f67cdf0daed93dfb78c197b9412ebed
Sergey <sergey.kulakov@inspirativ.com>
2017-05-05 17:47:33 +0300

    ST7_006 completed

---
+++ src/test/java/shopGophotoweb/tests/ST7_006.java
@@ -0,0 +1,89 @@
+package shopGophotoweb.tests;
+
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.CartPage;
+import shopGophotoweb.pages.CatalogPage;
+import shopGophotoweb.pages.ProductPage;
+import shopGophotoweb.pages.SuccessPage;
+import org.testng.annotations.BeforeMethod;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class ST7_006 extends BaseTest {
+    @BeforeMethod
+    public void setPreconditions(){
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+        logStep();
+        SettingsPage settingsPage=new SettingsPage();
+        Utilites.goToSidebarItem("Методы оплаты");
+
+        logStep();
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
+
+        logStep();
+        Utilites.goToSidebarItem("Методы доставки");
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.checkDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Курьер);
+    }
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep();
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        product1Page.goToCatalog();
+
+        logStep();
+        catalogPage.goToProductPage("product2");
+        ProductPage product2Page=new ProductPage();
+        product2Page.addProductToCart();
+        product2Page.goToCatalog();
+
+        logStep();
+        catalogPage.goToProductPage("product5");
+        ProductPage product5Page=new ProductPage();
+        product5Page.addProductToCart();
+        product5Page.goToCart();
+
+        logStep();
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep();
+        cartPage.selectDeliveryMethod(CartPage.DeliveryMethods.Курьер);
+        logger.info("Expected result: total price = 8 100.20 р.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"8 100.20 p.");
+
+        logStep();
+        cartPage.applyPromoCode("6");
+        logger.info("Expected result: total price = 7 292.20 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"7 292.20 p.");
+
+        logStep();
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        logger.info("The order was completed");
+    }
+}
