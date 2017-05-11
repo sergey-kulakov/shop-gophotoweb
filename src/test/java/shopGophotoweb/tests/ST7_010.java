e9e22309c4de3ae68ee44fc09ddb20b2a7cf5f04
Sergey <sergey.kulakov@inspirativ.com>
2017-05-11 14:19:02 +0300

    ST7_010 was completed

---
+++ src/test/java/shopGophotoweb/tests/ST7_010.java
@@ -0,0 +1,93 @@
+package shopGophotoweb.tests;
+
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.CartPage;
+import shopGophotoweb.pages.CatalogPage;
+import shopGophotoweb.pages.ProductPage;
+import shopGophotoweb.pages.SuccessPage;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class ST7_010 extends BaseTest {
+
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep();
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        product1Page.goToCatalog();
+
+        logStep();
+        catalogPage.goToProductPage("product3");
+        ProductPage product3Page=new ProductPage();
+        product3Page.addProductToCart();
+        product3Page.goToCart();
+
+        logStep();
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep();
+        cartPage.setProductCount("product1","3");
+
+        logStep();
+        cartPage.setProductCount("product3","4");
+        logger.info("Expected result: total price = 6 120.60 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"6 120.60 p.");
+
+        logStep();
+        cartPage.applyPromoCode("10");
+        assertTrue(cartPage.isPromocodeErrorDisplayed());
+
+        logStep();
+        cartPage.setProductCount("product1","4");
+        logger.info("Expected result: total price = 7 140.70 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"7 140.70 p.");
+
+        logStep();
+        cartPage.applyPromoCode("10");
+        assertTrue(cartPage.isPromocodeErrorDisplayed());
+
+        logStep();
+        cartPage.setProductCount("product1","6");
+        logger.info("Expected result: total price = 9 180.90 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"9 180.90 p.");
+
+        logStep();
+        cartPage.applyPromoCode("10");
+        logger.info("Expected result: total price = 8 271.90 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"8 271.90 p.");
+
+
+        logStep();
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        String orderNumber=successPage.getOrdrerNumber();
+        logger.info("The order was completed");
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+        logStep();
+        Utilites.goToMenuName("МАГАЗИН");
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToOrdersPage();
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"8 271.90 p.");
+    }
+}
