b7344d7764d2ac0e05447c7c4b03b8d869c21a54
Sergey <sergey.kulakov@inspirativ.com>
2017-05-11 17:15:18 +0300

    ST7_012 completed

---
+++ src/test/java/shopGophotoweb/tests/ST7_011.java
@@ -0,0 +1,73 @@
+package shopGophotoweb.tests;
+
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.CartPage;
+import shopGophotoweb.pages.CatalogPage;
+import shopGophotoweb.pages.ProductPage;
+import shopGophotoweb.pages.SuccessPage;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class ST7_011 extends BaseTest {
+
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep();
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        product1Page.goToCatalog();
+
+        logStep();
+        catalogPage.goToProductPage("product2");
+        ProductPage product2Page=new ProductPage();
+        product2Page.addProductToCart();
+        product2Page.goToCatalog();
+
+        logStep();
+        catalogPage.goToProductPage("product3");
+        ProductPage product3Page=new ProductPage();
+        product3Page.addProductToCart();
+        product3Page.goToCart();
+
+        logStep();
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logger.info("Expected result: total price = 4 565.20 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"4 565.20 p.");
+
+        logStep();
+        cartPage.applyPromoCode("11");
+        logger.info("Expected result: total price = 4 307.65 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"4 307.65 p.");
+
+
+        logStep();
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        String orderNumber=successPage.getOrdrerNumber();
+        logger.info("The order was completed");
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+        logStep();
+        Utilites.goToMenuName("МАГАЗИН");
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToOrdersPage();
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"4 307.65 p.");
+    }
+}
