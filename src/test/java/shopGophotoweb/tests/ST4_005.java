aa2c0af0dd513d49fb8151f04409d05add5e77e7
Sergey <sergey.kulakov@inspirativ.com>
2017-06-21 15:39:39 +0300

    4_005 was completed

---
+++ src/test/java/shopGophotoweb/tests/ST4_005.java
@@ -0,0 +1,115 @@
+package shopGophotoweb.tests;
+
+import org.testng.annotations.BeforeMethod;
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.*;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+public class ST4_005 extends BaseTest {
+    @BeforeMethod
+    public void setPreconditions() throws InterruptedException {
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+        logStep();
+        SettingsPage settingsPage=new SettingsPage();
+        Utilites.goToSidebarItem("Методы оплаты");
+
+        logStep();
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.unCheckAllMethods();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+
+        logStep();
+        Utilites.goToSidebarItem("Методы доставки");
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.unCheckAllMethods();
+        deliveryMethodsPage.checkDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Курьер);
+
+        logStep();
+        adminMainPage.goToSite();
+        Browser.switchWindow();
+
+
+    }
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep(1);
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+        ProductPage productPage=new ProductPage();
+        productPage.addProductToCart();
+        Menu.goToShopCatalog();
+
+        logStep(2);
+        catalogPage.goToProductPage("product2");
+        ProductPage product2Page=new ProductPage();
+        product2Page.addProductToCart();
+        Menu.goToCart();
+
+        logStep(3);
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep(4);
+        cartPage.selectPaymentMethod(CartPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+        logger.info("Expected result: total price = 4 130.30 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"4 130.30 p.");
+
+        //переключаемся в админку и скрываем метод оплаты
+        logStep(5);
+        Browser.switchWindow();
+        browser.navigate(Browser.getAdminPageUrl());
+        AdminMainPage adminMainPage=new AdminMainPage();
+        adminMainPage.goToShop();
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+        Utilites.goToSidebarItem("Методы оплаты");
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.unCheckPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+
+        //переключаемся на сайт
+        logStep(6);
+        Browser.switchWindow();
+        cartPage.clickSubmit();
+        assertTrue(cartPage.isErrorPaymentMethodUnavailableDislayed());
+        assertTrue(cartPage.isErrorTotalOrderSumChangedDisplayed());
+
+        logStep(7);
+        logger.info("Expected result: total price = 4 050.10 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"4 050.10 p.");
+        cartPage.clickSubmit();
+
+        SuccessPage successPage=new SuccessPage();
+        successPage.isThanksForOrderMessageDisplayed();
+        String orderNumber=successPage.getOrdrerNumber();
+        logger.info("The order was completed");
+
+        logStep(7);
+        browser.navigate(Browser.getAdminPageUrl());
+        adminMainPage.goToShop();
+        Utilites.goToMenuName("МАГАЗИН");
+        adminProductsPage.goToOrdersPage();
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"4 050.10 p.");
+
+
+    }}
