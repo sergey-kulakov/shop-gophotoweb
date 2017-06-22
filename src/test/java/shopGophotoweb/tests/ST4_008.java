a9f2adf70afa9aa3973791408332e7cfbbcba880
Sergey <sergey.kulakov@inspirativ.com>
2017-06-22 16:09:26 +0300

    4_001 fixed

--- src/test/java/shopGophotoweb/tests/ST4_008.java
+++ src/test/java/shopGophotoweb/tests/ST4_008.java
@@ -8,7 +8,7 @@ import webdriver.Browser;
 
 import static org.testng.Assert.assertTrue;
 
-public class ST4_001 extends BaseTest {
+public class ST4_008 extends BaseTest {
     @BeforeMethod
     public void setPreconditions() throws InterruptedException {
 
@@ -31,6 +31,8 @@ public class ST4_001 extends BaseTest {
         logStep();
         PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
         paymetsMethodsPage.unCheckAllMethods();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
 
         logStep();
         Utilites.goToSidebarItem("Методы доставки");
@@ -50,23 +52,23 @@ public class ST4_001 extends BaseTest {
     @Override
     public void runTest() throws InterruptedException {
 
-        logStep();
+        logStep(1);
         CatalogPage catalogPage=new CatalogPage();
         catalogPage.goToProductPage("product1");
         ProductPage productPage=new ProductPage();
         productPage.addProductToCart();
         Menu.goToCart();
 
-        logStep();
+        logStep(2);
         CartPage cartPage=new CartPage();
         cartPage.selectDeliveryMethod(CartPage.DeliveryMethods.Курьер);
         cartPage.fillInFields("test", "test", "tt@tt.tt");
-        logger.info("Expected result: total price = 2 000 p.");
+        logger.info("Expected result: total price = 2 020 p.");
         logger.info("Actual result: total price = "+cartPage.getTotalPrice());
-        assertEquals(cartPage.getTotalPrice(),"2 000 p.");
+        assertEquals(cartPage.getTotalPrice(),"2 020 p.");
 
         //переключаемся в админку и скрываем метод доставки
-        logStep();
+        logStep(3);
         Browser.switchWindow();
         browser.navigate(Browser.getAdminPageUrl());
         AdminMainPage adminMainPage=new AdminMainPage();
@@ -74,45 +76,29 @@ public class ST4_001 extends BaseTest {
         AdminProductsPage adminProductsPage=new AdminProductsPage();
         adminProductsPage.goToSettingsPage();
 
-        logStep();
         Utilites.goToSidebarItem("Методы доставки");
         DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
         deliveryMethodsPage.unCheckDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Курьер);
 
         //переключаемся на сайт
-        logStep();
+        logStep(4);
         Browser.switchWindow();
         cartPage.clickSubmit();
 
-        logStep();
         assertTrue(cartPage.isErrorDeliveryMethodUnavailableDislayed());
         assertTrue(cartPage.isErrorTotalOrderSumChangedDisplayed());
-
-        logStep();
-        cartPage.selectDeliveryMethod(CartPage.DeliveryMethods.Самовывоз);
-
-        logStep();
         logger.info("Expected result: total price = 1 000 p.");
         logger.info("Actual result: total price = "+cartPage.getTotalPrice());
         assertEquals(cartPage.getTotalPrice(),"1 000 p.");
 
-
-        //переключаемся в админку и скрываем метод доставки
-        logStep();
-        Browser.switchWindow();
-        deliveryMethodsPage.unCheckDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Самовывоз);
-
-        //переключаемся на сайт
-        logStep();
-        Browser.switchWindow();
+        logStep(5);
+        cartPage.selectDeliveryMethod(CartPage.DeliveryMethods.Самовывоз);
+        logger.info("Expected result: total price = 1 010 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"1 010 p.");
         cartPage.clickSubmit();
 
-        logStep();
-        assertTrue(cartPage.isErrorDeliveryMethodUnavailableDislayed());
-
-
-        logStep();
-        cartPage.clickSubmit();
+        logStep(6);
         SuccessPage successPage=new SuccessPage();
         successPage.isThanksForOrderMessageDisplayed();
         String orderNumber=successPage.getOrdrerNumber();
@@ -125,7 +111,7 @@ public class ST4_001 extends BaseTest {
         Utilites.goToMenuName("МАГАЗИН");
         adminProductsPage.goToOrdersPage();
         OrdersPage ordersPage=new OrdersPage();
-        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"1 000 p.");
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"1 010 p.");
 
 
     }}
