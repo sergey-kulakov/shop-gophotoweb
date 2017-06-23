961df17cd064a5c8de4e680b1ac2ac2576e45e0f
Sergey <sergey.kulakov@inspirativ.com>
2017-06-23 15:00:57 +0300

    ST5_001 was completed

--- src/test/java/shopGophotoweb/adminPages/DeliveryMethodPage.java
+++ src/test/java/shopGophotoweb/adminPages/DeliveryMethodPage.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.By;
 import webdriver.BaseForm;
 import webdriver.elements.Button;
 import webdriver.elements.CheckBox;
+import webdriver.elements.TextBox;
 
 
 public class DeliveryMethodPage extends BaseForm {
     private String locCheckboxPaymetnMethod="//span[contains(text(),'%S')]/preceding-sibling::span";
     private Button btnSubmit=new Button(By.xpath("//input[@value='Сохранить']"),"Submit button");
+    private TextBox txbDeliveryTax=new TextBox(By.xpath("//input[@id='delivery-method-tax_per_order']"), "Delivery method tax textbox");
+    private TextBox txbAdditionalDeliveryTax=new TextBox(By.id("delivery-method-tax_per_order_item"),"Delivery method additional tax textbox");
     public DeliveryMethodPage(){super(By.xpath("//div[contains(text(),'Название метода доставки')]"),"Delivery methods page");}
 
     public void checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods paymentMethodName){
@@ -24,6 +27,20 @@ public class DeliveryMethodPage extends BaseForm {
         System.out.println("test");
         chkPaymentMethodVisibility.waitForIsElementPresent();
         chkPaymentMethodVisibility.uncheck();
+        btnSubmit.click();
+    }
+    public  void setDeliveryTax(String deliveryTax){
+
+        txbDeliveryTax.waitForIsElementPresent();
+        txbDeliveryTax.setText(deliveryTax);
+
+    }
+    public void setAdditionalDeliveryTax(String deliveryTax){
+        txbAdditionalDeliveryTax.waitForIsElementPresent();
+        txbAdditionalDeliveryTax.setText(deliveryTax);
+
+    }
+    public void  clickSave(){
         btnSubmit.click();
     }
 }
---
+++ src/test/java/shopGophotoweb/tests/ST5_001.java
@@ -0,0 +1,120 @@
+package shopGophotoweb.tests;
+
+import org.testng.annotations.BeforeMethod;
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.*;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertTrue;
+
+public class ST5_001 extends BaseTest {
+    @BeforeMethod
+    public void setPreconditions() throws InterruptedException {
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+        logStep();
+        SettingsPage settingsPage=new SettingsPage();
+        Utilites.goToSidebarItem("Методы оплаты");
+
+        logStep();
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.unCheckAllMethods();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
+
+        logStep();
+        Utilites.goToSidebarItem("Методы доставки");
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.unCheckAllMethods();
+        deliveryMethodsPage.checkDeliveryMethodVisible(DeliveryMethodsPage.DeliveryMethods.Курьер);
+
+        logStep();
+        adminMainPage.goToSite();
+        Browser.switchWindow();
+
+
+
+
+    }
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep(1);
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+        ProductPage productPage=new ProductPage();
+        productPage.addProductToCart();
+        Menu.goToCart();
+
+        logStep(2);
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep(3);
+        logger.info("Expected result: total price = 2 020 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"2 020 p.");
+
+        //переключаемся в админку и меняем стоимость доставки
+        logStep(4);
+        Browser.switchWindow();
+        browser.navigate(Browser.getAdminPageUrl());
+        AdminMainPage adminMainPage=new AdminMainPage();
+        adminMainPage.goToShop();
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+
+        Utilites.goToSidebarItem("Методы доставки");
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.goToDeliveryMethod(DeliveryMethodsPage.DeliveryMethods.Курьер);
+        DeliveryMethodPage deliveryMethodPage=new DeliveryMethodPage();
+        deliveryMethodPage.setDeliveryTax("0");
+        deliveryMethodPage.setAdditionalDeliveryTax("0");
+        deliveryMethodPage.clickSave();
+
+        //переключаемся на сайт
+        logStep();
+        Browser.switchWindow();
+        cartPage.clickSubmit();
+
+        logStep();
+        assertTrue(cartPage.isErrorTotalOrderSumChangedDisplayed());
+        logger.info("Expected result: total price = 1 010 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"1 010 p.");
+
+        logStep();
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        successPage.isThanksForOrderMessageDisplayed();
+        String orderNumber=successPage.getOrdrerNumber();
+        logger.info("The order was completed");
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        adminMainPage.goToShop();
+        logStep();
+        Utilites.goToMenuName("МАГАЗИН");
+        adminProductsPage.goToOrdersPage();
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"1 010 p.");
+
+        Browser.switchWindow();
+        deliveryMethodPage.setDeliveryTax("1000");
+        deliveryMethodPage.setAdditionalDeliveryTax("10");
+        deliveryMethodPage.clickSave();
+
+
+    }}
