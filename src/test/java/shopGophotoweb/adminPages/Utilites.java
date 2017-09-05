e1ee0790d30917cf88cba8bc172e9494a2ae39da
Sergey <sergey.kulakov@inspirativ.com>
2017-09-05 17:02:48 +0300

    ST5_001 fix

--- src/test/java/shopGophotoweb/adminPages/Utilites.java
+++ src/test/java/shopGophotoweb/adminPages/Utilites.java
@@ -5,8 +5,9 @@ import webdriver.elements.Button;
 
 
 public class Utilites {
-    static String locMenuItem="//ul[@class='clearfix']/li/a/span[contains(text(),'%s')]";
-    static String locSidear="//ul[@class='nav navbar-nav']/li/a[contains(text(),'%s')]";
+    private static String locMenuItem="//ul[@class='clearfix']/li/a/span[contains(text(),'%s')]";
+    private static String locSidear="//ul[@class='nav navbar-nav']/li/a[contains(text(),'%s')]";
+    private static Button btnLogout=new Button(By.xpath("//a[contains(@href,'logout')]/span[contains(text(),'Выход')]"),"Logout button");
 
     public static void goToMenuName(String menuItemName){
         Button btnMenuItem=new Button(By.xpath(String.format(locMenuItem,menuItemName)),"Menu item with name"+menuItemName);
@@ -16,5 +17,9 @@ public class Utilites {
     public static void goToSidebarItem(String itemName){
         Button btnSidebar=new Button(By.xpath(String.format(locSidear,itemName)),"Sidebar item");
         btnSidebar.click();
+    }
+    public static void logout(){
+        btnLogout.waitForIsElementPresent();
+        btnLogout.click();
     }
 }
--- src/test/java/shopGophotoweb/tests/ST5_001.java
+++ src/test/java/shopGophotoweb/tests/ST5_001.java
@@ -1,6 +1,7 @@
 package shopGophotoweb.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import shopGophotoweb.adminPages.*;
 import shopGophotoweb.pages.*;
 import webdriver.BaseTest;
@@ -9,6 +10,35 @@ import webdriver.Browser;
 import static org.testng.Assert.assertTrue;
 
 public class ST5_001 extends BaseTest {
+    @BeforeSuite
+    public void fixPreconditions()
+    {
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        adminProductsPage.goToSettingsPage();
+
+
+        Utilites.goToSidebarItem("Методы доставки");
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.goToDeliveryMethod(DeliveryMethodsPage.DeliveryMethods.Курьер);
+        DeliveryMethodPage deliveryMethodPage=new DeliveryMethodPage();
+        deliveryMethodPage.setDeliveryTax("1000");
+        deliveryMethodPage.setAdditionalDeliveryTax("10");
+        deliveryMethodPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.МОЙ_ВИД_ОПЛАТЫ_С_КОМИССИЕЙ_1);
+        deliveryMethodPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+        deliveryMethodPage.clickSave();
+        Utilites.logout();
+    }
+
     @BeforeMethod
     public void setPreconditions() throws InterruptedException {
 
@@ -66,52 +96,56 @@ public class ST5_001 extends BaseTest {
         logger.info("Actual result: total price = "+cartPage.getTotalPrice());
         assertEquals(cartPage.getTotalPrice(),"2 020 p.");
 
-        //переключаемся в админку и меняем стоимость доставки
-        logStep(4);
-        Browser.switchWindow();
-        browser.navigate(Browser.getAdminPageUrl());
-        AdminMainPage adminMainPage=new AdminMainPage();
-        adminMainPage.goToShop();
-        AdminProductsPage adminProductsPage=new AdminProductsPage();
-        adminProductsPage.goToSettingsPage();
+
+            //переключаемся в админку и меняем стоимость доставки
+            logStep(4);
+            Browser.switchWindow();
+            browser.navigate(Browser.getAdminPageUrl());
+            AdminMainPage adminMainPage=new AdminMainPage();
+            adminMainPage.goToShop();
+            AdminProductsPage adminProductsPage=new AdminProductsPage();
+            adminProductsPage.goToSettingsPage();
 
 
-        Utilites.goToSidebarItem("Методы доставки");
-        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
-        deliveryMethodsPage.goToDeliveryMethod(DeliveryMethodsPage.DeliveryMethods.Курьер);
-        DeliveryMethodPage deliveryMethodPage=new DeliveryMethodPage();
-        deliveryMethodPage.setDeliveryTax("0");
-        deliveryMethodPage.setAdditionalDeliveryTax("0");
-        deliveryMethodPage.clickSave();
+            Utilites.goToSidebarItem("Методы доставки");
+            DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+            deliveryMethodsPage.goToDeliveryMethod(DeliveryMethodsPage.DeliveryMethods.Курьер);
+            DeliveryMethodPage deliveryMethodPage=new DeliveryMethodPage();
+            deliveryMethodPage.setDeliveryTax("0");
+            deliveryMethodPage.setAdditionalDeliveryTax("0");
+            deliveryMethodPage.clickSave();
 
-        //переключаемся на сайт
-        logStep(5);
-        Browser.switchWindow();
-        cartPage.clickSubmit();
+            //переключаемся на сайт
+            logStep(5);
+            Browser.switchWindow();
+            cartPage.clickSubmit();
 
-        assertTrue(cartPage.isErrorTotalOrderSumChangedDisplayed());
-        logger.info("Expected result: total price = 1 010 p.");
-        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
-        assertEquals(cartPage.getTotalPrice(),"1 010 p.");
+            assertTrue(cartPage.isErrorTotalOrderSumChangedDisplayed());
+            logger.info("Expected result: total price = 1 010 p.");
+            logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+            assertEquals(cartPage.getTotalPrice(),"1 010 p.");
 
-        cartPage.clickSubmit();
-        SuccessPage successPage=new SuccessPage();
-        successPage.isThanksForOrderMessageDisplayed();
-        String orderNumber=successPage.getOrdrerNumber();
-        logger.info("The order was completed");
-        browser.navigate(Browser.getAdminPageUrl());
-        adminMainPage.goToShop();
-        logStep();
-        Utilites.goToMenuName("МАГАЗИН");
-        adminProductsPage.goToOrdersPage();
-        OrdersPage ordersPage=new OrdersPage();
-        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"1 010 p.");
+            cartPage.clickSubmit();
+            SuccessPage successPage=new SuccessPage();
+            successPage.isThanksForOrderMessageDisplayed();
+            String orderNumber=successPage.getOrdrerNumber();
+            logger.info("The order was completed");
+            browser.navigate(Browser.getAdminPageUrl());
+            adminMainPage.goToShop();
+            logStep();
+            Utilites.goToMenuName("МАГАЗИН");
+            adminProductsPage.goToOrdersPage();
+            OrdersPage ordersPage=new OrdersPage();
+            assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"1 010 p.");
+
+
 
         logStep("Postconditions");
         Browser.switchWindow();
         deliveryMethodPage.setDeliveryTax("1000");
         deliveryMethodPage.setAdditionalDeliveryTax("10");
         deliveryMethodPage.clickSave();
+
 
 
     }}
