809fc00644b425d9b4f7d05acf97ffcae7533e50
Sergey <sergey.kulakov@inspirativ.com>
2017-10-11 17:54:59 +0300

    Cart icon locator fix

--- src/test/java/shopGophotoweb/pages/Menu.java
+++ src/test/java/shopGophotoweb/pages/Menu.java
@@ -6,7 +6,7 @@ import webdriver.elements.Button;
 
 public class Menu {
     private static Button btnShop=new Button(By.xpath("//a[contains(text(),'МАГАЗИН')]"),"Go to shop catalog button");
-    private static Button btnGoToCart = new Button(By.xpath("//a[@id='shop-cart-widget']"),"goToCart");
+    private static Button btnGoToCart = new Button(By.xpath("//a[@data-id='shop-cart-widget']"),"goToCart");
 
     public static void goToShopCatalog(){
         btnShop.waitForIsElementPresent();
