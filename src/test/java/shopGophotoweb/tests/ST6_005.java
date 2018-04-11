47e887ce9a5be7e532422ceb485dccba57c1a907
Sergey <sergey.kulakov@inspirativ.com>
2018-04-11 16:29:26 +0300

    ST6_005 was completed

---
+++ src/test/java/shopGophotoweb/tests/ST6_005.java
@@ -0,0 +1,97 @@
+package shopGophotoweb.tests;
+
+import org.testng.annotations.BeforeMethod;
+import shopGophotoweb.adminPages.*;
+import shopGophotoweb.pages.*;
+import webdriver.BaseTest;
+import webdriver.Browser;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+
+public class ST6_005 extends BaseTest {
+    @BeforeMethod
+    public void setPreconditions(){
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        LoginPage loginPage=new LoginPage();
+        loginPage.login();
+
+        logStep();
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+
+        AdminProductsPage adminProductsPage=new AdminProductsPage();
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Продажи);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.оплаты);
+
+        logStep();
+        PaymetsMethodsPage paymetsMethodsPage=new PaymetsMethodsPage();
+        paymetsMethodsPage.unCheckAllMethods();
+        paymetsMethodsPage.checkPaymentMethodVisible(PaymetsMethodsPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+
+        logStep();
+        Utilites.goToSidebarItem(Utilites.SidebarItems.доставки);
+        DeliveryMethodsPage deliveryMethodsPage=new DeliveryMethodsPage();
+        deliveryMethodsPage.unCheckAllMethods();
+
+
+
+
+    }
+    @Override
+    public void runTest() throws InterruptedException {
+
+        logStep(1);
+        CatalogPage catalogPage=new CatalogPage();
+        catalogPage.goToProductPage("product1");
+
+        ProductPage product1Page=new ProductPage();
+        product1Page.addProductToCart();
+        Menu.goToCart();
+
+        logStep(2);
+        CartPage cartPage=new CartPage();
+        cartPage.fillInFields("test", "test", "tt@tt.tt");
+
+        logStep(3);
+        cartPage.setProductCount("product1","10");
+        logger.info("Expected result: total price = 10 000 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"10 000 p.");
+
+        logStep(4);
+        cartPage.selectPaymentMethod(CartPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3);
+        logger.info("Expected result: total price = 10 300 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"10 300 p.");
+
+        logStep(5);
+        cartPage.applyPromoCode("21");
+        logger.info("Expected result: total price = 0 p.");
+        logger.info("Actual result: total price = "+cartPage.getTotalPrice());
+        assertEquals(cartPage.getTotalPrice(),"0 p.");
+        assertFalse(cartPage.isPaymentMethodDisplayed(CartPage.PaymentMethods.ЧЕРЕЗ_СИСТЕМУ_ЯНДЕКС_ДЕНЬГИ_С_КОММИСИЕЙ_3));
+
+
+        logStep();
+        cartPage.clickSubmit();
+        SuccessPage successPage=new SuccessPage();
+        assertTrue(successPage.isThanksForOrderMessageDisplayed());
+        String orderNumber=successPage.getOrderNumber();
+        logger.info("The order was completed");
+
+        logStep();
+        browser.navigate(Browser.getAdminPageUrl());
+        AdminMainPage adminMainPage = new AdminMainPage();
+        adminMainPage.goToShop();
+        logStep();
+        Utilites.goToMenuName("МАГАЗИН");
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Продажи);
+        Utilites.goToSidebarItem(Utilites.SidebarItems.Заказы);
+        OrdersPage ordersPage=new OrdersPage();
+        assertEquals(ordersPage.getOrderTotalPrice(orderNumber),"0 p.");
+    }
+}
